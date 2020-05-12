21338fc7d90e5d37bb6fe8902d6b50e8ef9b2a1c
FlorianGERMAIN <florian.germain@free.fr>
2020-05-12 16:32:06 +0200

    Ajout du projet de TU ( Tests Unitaires)

---
+++ MathHelperApp/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MathHelperApp;
+using System.Security.AccessControl;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // Arrange
+            var calc = new ConverterLibrary();
+
+            // Act
+            var result = calc.GetInt("42");
+
+            //Assert
+            Assert.AreEqual(42, result);
+
+
+
+        }
+
+
+
+
+
+
+
+
+    }
+}
